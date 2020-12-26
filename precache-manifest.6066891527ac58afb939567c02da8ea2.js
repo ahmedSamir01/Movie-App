7c3bf590d0d6f6319878fd195bac2042377718f6
ahmed samir <asproducer80@gmail.com>
2020-12-26 03:24:56 +0200

    Updates

---
+++ precache-manifest.6066891527ac58afb939567c02da8ea2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a47a4e4c74c78e80acb7",
+    "url": "/Movie-App/static/css/main.b293d55c.chunk.css"
+  },
+  {
+    "revision": "a47a4e4c74c78e80acb7",
+    "url": "/Movie-App/static/js/main.6dde1a76.chunk.js"
+  },
+  {
+    "revision": "7198c6a1e3704149705a",
+    "url": "/Movie-App/static/js/runtime~main.c4615995.js"
+  },
+  {
+    "revision": "38527e258552a99426a6",
+    "url": "/Movie-App/static/js/2.eb4af692.chunk.js"
+  },
+  {
+    "revision": "a60f11da98ae62668cfbb96f9c53460d",
+    "url": "/Movie-App/index.html"
+  }
+];
